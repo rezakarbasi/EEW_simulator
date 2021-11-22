2174c68b904931286f1a25943baa80303ff12761
Reza Karbasi <rezakarbasi@Rezas-MacBook-Pro.local>
2021-11-22 17:28:15 +0330

    DataGenFromFile is going on ...

--- Data/gen_data/DataGenerationFromFile.py
+++ Data/gen_data/DataGenerationFromFile.py
@@ -1,10 +1,19 @@
 import numpy as np
 import datetime
+import pandas as pd
 
 import sys
 sys.path.append('./')
 from Objects.objects import PLACES,STATION_RECORD,EARTHQUAKE_OBJ,PARAMETER_TYPE,UI_OBJ
 from Functions import get_data,remove_bias
+
+def select_sequentially(row):
+    out = []
+    for i in range(len(row)):
+        if str(row[i])=='nan':
+            break
+        out.append(row[i])
+    return out
 
 class DATA_GENERATOR_FROM_FILE(UI_OBJ):
     def __str__(self):
@@ -57,3 +66,44 @@ class DATA_GENERATOR_FROM_FILE(UI_OBJ):
             signal,_,stream = get_data(self.ewSignal)
             self.signalFreq = stream.stats.sampling_rate
             self.ewSignal = remove_bias(signal)
+
+        c1H = pd.read_excel(self.configPath,sheet_name="C1-HORIZONTAL", index_col=None, header=None)
+        c1H = np.array(c1H)
+        longitudes = select_sequentially(c1H[1,2:])
+        latitudes = select_sequentially(c1H[2:,1])
+        self.latitudes = latitudes
+        self.longitudes = longitudes
+        self.c1H = c1H[2:2+len(latitudes),2:2+len(longitudes)]
+
+        c2H = pd.read_excel(self.configPath,sheet_name="C2-HORIZONTAL", index_col=None, header=None)
+        self.c2H = np.array(c2H)[2:2+len(latitudes),2:2+len(longitudes)]
+
+        c3H = pd.read_excel(self.configPath,sheet_name="C3-HORIZONTAL", index_col=None, header=None)
+        self.c3H = np.array(c3H)[2:2+len(latitudes),2:2+len(longitudes)]
+
+        c1V = pd.read_excel(self.configPath,sheet_name="C1-VERTICAL", index_col=None, header=None)
+        self.c1V = np.array(c1V)[2:2+len(latitudes),2:2+len(longitudes)]
+
+        c2V = pd.read_excel(self.configPath,sheet_name="C2-VERTICAL", index_col=None, header=None)
+        self.c2V = np.array(c2V)[2:2+len(latitudes),2:2+len(longitudes)]
+
+        c3V = pd.read_excel(self.configPath,sheet_name="C3-VERTICAL", index_col=None, header=None)
+        self.c3V = np.array(c3V)[2:2+len(latitudes),2:2+len(longitudes)]
+
+        self.stations = pd.read_excel(self.configPath,sheet_name="stations",index_col=None)
+        self.info = pd.read_excel(self.configPath,sheet_name="earthquake",index_col=None)
+
+    def return_idx(self,lat,lon):
+        lat_idx = np.where(self.latitudes>lat)[0]
+        if len(lat_idx)>0:
+            lat_idx = lat_idx[0]
+        else : 
+            lat_idx = len(self.latitudes)-1
+
+        lon_idx = np.where(self.longitudes>lon)[0]
+        if len(lon_idx)>0:
+            lon_idx = lon_idx[0]
+        else : 
+            lon_idx = len(self.longitudes)-1
+
+        return (lat_idx,lon_idx)
